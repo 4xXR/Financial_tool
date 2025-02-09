9815aeab3556afb3ecaf906b5bc7498fe35bb4e7
Daniel Urrutia <danyurru92@gmail.com>
2025-02-09 12:16:00 -0500

    Fixing datetime atributes

--- src/export_data.py
+++ src/export_data.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def export_to_csv(data, filename="../data/financial_data.csv"):
     """Exports financial data to a timestamped CSV file."""
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"../datafinancial_data_{timestamp}.csv"
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"../data/financial_data_{timestamp}.csv"
 
     df = pd.DataFrame(data)
     df.to_csv(filename, index=False)
