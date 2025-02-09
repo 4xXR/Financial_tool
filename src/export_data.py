f2743c5170ca3149e8b0ad087564ba80ebbf40ab
Daniel Urrutia <danyurru92@gmail.com>
2025-02-08 20:47:17 -0500

    Generating a new file each time script runs with current date to the filename

--- src/export_data.py
+++ src/export_data.py
@@ -1,7 +1,11 @@
+import datetime
 import pandas as pd
 
 def export_to_csv(data, filename="../data/financial_data.csv"):
-    """Exports financial data to a CSV file."""
+    """Exports financial data to a timestamped CSV file."""
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"../datafinancial_data_{timestamp}.csv"
+
     df = pd.DataFrame(data)
     df.to_csv(filename, index=False)
     print(f"Data succesfully saved to {filename}")
