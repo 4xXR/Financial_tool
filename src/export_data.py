b5eff0da51241285b7e1ff322f2eeea72282ec3b
Daniel Urrutia <danyurru92@gmail.com>
2025-05-15 21:19:41 -0400

    Add: Intrinsic Value based on Peer PER row to output table

--- src/export_data.py
+++ src/export_data.py
@@ -10,6 +10,21 @@ def export_to_csv(data, filename="../data/financial_data.csv"):
     df = df.set_index("Company").T  # Set tickers as columns, financial metrics as rows
     df.index.name = "RATIOS"
 
+    # Calcula row: Intrinsic Value based on Peer PER
+    try:
+        prices = df.loc["PRICE"] # Extrae la fila de precios actuales, por ticker
+        pers = df.loc["PER (Current FMP)"] # Extrae la fila de PER actuales, por ticker
+        avg_per = pers.mean() # Calcula el PER promedio de todos los tickers
+
+        # Calculation: Intrinsic Value using average PER
+        intrinsic_by_peer = (prices * avg_per) / pers
+
+        # Inserts the result as a new row into the DataFrame
+        df.loc["Intrinsic Value based on Peer PER"] = intrinsic_by_peer
+
+    except Exception as e:
+        print(f"Error calculating Intrinsic Value based on Peer PER: {e}")
+
     # Adding 'AVERAGAE' column
     df['AVERAGE']=df.mean(axis=1, numeric_only=True)
 
